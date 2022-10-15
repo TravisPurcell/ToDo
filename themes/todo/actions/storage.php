5718064028636a5e14cdf1b17321d57068f081eb
TravisPurcell <travis.n.purcell@gmail.com>
2022-10-15 10:11:11 -0600

    Completed action

--- themes/todo/actions/storage.php
+++ themes/todo/actions/storage.php
@@ -2,10 +2,15 @@
     $task = $_POST['task'];
 
  	//Connect to Database
-     $dbhost = 'localhost';
-     $dbuser = 'wp';
-     $dbpass = 'wp';
-     $dbname = 'todo';
+		// $dbhost = 'localhost';
+		// $dbuser = 'tnpportf_WPK9D';
+		// $dbpass = 'Gr-l~=L~*hQU';
+		// $dbname = 'tnpportf_WPK9D';
+
+		$dbhost = 'localhost';
+		$dbuser = 'wp';
+		$dbpass = 'wp';
+		$dbname = 'todo';
      $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
      if($conn->connect_errno ) {
